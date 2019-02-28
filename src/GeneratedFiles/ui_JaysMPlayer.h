11490cdae07df2a8f2cdd5aa13f3846d630825bd
Jay Heng <jie.heng@nxp.com>
2019-02-28 16:21:22 +0800

    [UI] Add two Widgets label and textEdit (to show info)
    - Click JaysMPlayer.ui file in VS to open Qt Designer, then design the UI and save it.
    - Use uic.exe to convert xxx.ui to ui_xxx.h
    
    Signed-off-by: Jay Heng <jie.heng@nxp.com>

--- src/GeneratedFiles/ui_JaysMPlayer.h
+++ src/GeneratedFiles/ui_JaysMPlayer.h
@@ -11,9 +11,11 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
@@ -22,9 +24,11 @@ QT_BEGIN_NAMESPACE
 class Ui_JaysMPlayerClass
 {
 public:
+    QWidget *centralWidget;
+    QLabel *label_ffmpegInfo;
+    QTextEdit *textEdit_ffmpegInfo;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
-    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *JaysMPlayerClass)
@@ -32,15 +36,22 @@ public:
         if (JaysMPlayerClass->objectName().isEmpty())
             JaysMPlayerClass->setObjectName(QString::fromUtf8("JaysMPlayerClass"));
         JaysMPlayerClass->resize(600, 400);
+        centralWidget = new QWidget(JaysMPlayerClass);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        label_ffmpegInfo = new QLabel(centralWidget);
+        label_ffmpegInfo->setObjectName(QString::fromUtf8("label_ffmpegInfo"));
+        label_ffmpegInfo->setGeometry(QRect(40, 20, 71, 16));
+        textEdit_ffmpegInfo = new QTextEdit(centralWidget);
+        textEdit_ffmpegInfo->setObjectName(QString::fromUtf8("textEdit_ffmpegInfo"));
+        textEdit_ffmpegInfo->setGeometry(QRect(30, 40, 541, 281));
+        JaysMPlayerClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(JaysMPlayerClass);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 21));
         JaysMPlayerClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(JaysMPlayerClass);
         mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
-        JaysMPlayerClass->addToolBar(mainToolBar);
-        centralWidget = new QWidget(JaysMPlayerClass);
-        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
-        JaysMPlayerClass->setCentralWidget(centralWidget);
+        JaysMPlayerClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(JaysMPlayerClass);
         statusBar->setObjectName(QString::fromUtf8("statusBar"));
         JaysMPlayerClass->setStatusBar(statusBar);
@@ -53,6 +64,7 @@ public:
     void retranslateUi(QMainWindow *JaysMPlayerClass)
     {
         JaysMPlayerClass->setWindowTitle(QApplication::translate("JaysMPlayerClass", "JaysMPlayer", nullptr));
+        label_ffmpegInfo->setText(QApplication::translate("JaysMPlayerClass", "FFmpeg Info:", nullptr));
     } // retranslateUi
 
 };
